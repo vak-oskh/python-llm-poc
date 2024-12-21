cc68418991eae1bcaee1e37551a911d984fba58b
Vyacheslav Khudan <vyacheslav.oskh@gmail.com>
2024-12-21 14:26:07 -0700

    Initial interface for the alarm function

---
+++ backend/functions/alarm.py
@@ -0,0 +1,12 @@
+# Set an alarm which is based on Linux crontab.
+
+# pip install python-crontab
+from crontab import CronTab
+
+def set_alarm(time):
+    """Sets an alarm."""
+    pass
+
+def cancel_alarm(time):
+    """Cancels an alarm."""
+    pass
